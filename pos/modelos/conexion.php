5c12d3c2a6c2afb91aaf515862d519e06110f736
Cristian Bahamon <bahamon2803@gmail.com>
2022-06-21 22:11:52 -0500

    Fix bugs and update DB

--- pos/modelos/conexion.php
+++ pos/modelos/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=pos",
+		$link = new PDO("mysql:host=127.0.0.1:33065;dbname=pos",
 			            "root",
 			            "");
 
